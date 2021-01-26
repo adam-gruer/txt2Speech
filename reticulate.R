f70bca8ddfb33420f8cdb922a41351ed6e8f0a00
Adam Gruer <Adam.Gruer@epa.vic.gov.au>
2021-01-27 01:03:58 +1100

    Comment out conda env code from R file

--- reticulate.R
+++ reticulate.R
@@ -1,9 +1,10 @@
 library(reticulate)
 library(tidyverse)
 
-use_condaenv(here::here("conda-env"), required = TRUE)
+#use_condaenv(here::here("conda-env"), required = TRUE)
 
 os <- import("os")
 pysttx3 <- import("pyttsx3")
+
 
  
